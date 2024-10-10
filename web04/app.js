b453ea0b72dd295c81df05be214247a8ebe91eba
baejeong-eun <lovelyeun0914@gmail.com>
2024-10-10 23:23:13 +0900

    수정

--- web04/app.js
+++ web04/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./web04/routes');
-var usersRouter = require('./web04/routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/kakao', require('./web04/routes/kakao'))
+app.use('/kakao', require('./routes/kakao'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
